abb9559198da8c8107cd6ed615347410418b7c2e
carloswimmer <coiote777@gmail.com>
2018-02-17 23:10:05 -0300

    Aula 5. Atividade 2.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,20 +7,35 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+
+        this._negociacoesView.update();
     }
 
     adiciona(event) {
 
         event.preventDefault();
 
-        let helper = new DateHelper();
-        
-        let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
 
-        console.log(helper.dataParaTexto(negociacao.data));
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
--- aluraframe/client/js/app/helpers/dateHelper.js
+++ aluraframe/client/js/app/helpers/dateHelper.js
@@ -1,11 +1,17 @@
 class DateHelper {
 
-    textoParaData(texto) {
+    constructor() {
+        throw new Error('DateHelper não pode ser instanciada');
+    }
+
+    static textoParaData(texto) {
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('A data deve ser aaaa-mm-dd.');
 
         return new Date(texto.split('-'));
     }
 
-    dataParaTexto(data) {
+    static dataParaTexto(data) {
 
         return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
